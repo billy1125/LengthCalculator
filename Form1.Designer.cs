4870ed8cfac7bed691f06fc54f466f0b582be409
Cho-Hsun Lu <billy1125@gmail.com>
2025-03-10 14:31:35 +0800

    控制項名稱修改
    清除按鍵：btnAllClear
    公分輸入文字框：txtCM
    公尺輸入文字框：txtM
    公里輸入文字框：txtKM
    英吋輸入文字框：txtIn
    英呎輸入文字框：txtFt
    英碼輸入文字框：txtYard
    說明文字輸入文字框：txtInfo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,19 +30,19 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnAllClear = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtCM = new System.Windows.Forms.TextBox();
+            this.txtM = new System.Windows.Forms.TextBox();
+            this.txtKM = new System.Windows.Forms.TextBox();
+            this.txtYard = new System.Windows.Forms.TextBox();
+            this.txtFt = new System.Windows.Forms.TextBox();
+            this.txtIn = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -65,15 +65,15 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "公尺";
             // 
-            // button1
+            // btnAllClear
             // 
-            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(42, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 48);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "清除";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAllClear.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAllClear.Location = new System.Drawing.Point(42, 45);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(86, 48);
+            this.btnAllClear.TabIndex = 2;
+            this.btnAllClear.Text = "清除";
+            this.btnAllClear.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
@@ -125,73 +125,73 @@
             this.label7.TabIndex = 7;
             this.label7.Text = "說明文字";
             // 
-            // textBox1
+            // txtCM
             // 
-            this.textBox1.Location = new System.Drawing.Point(42, 143);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(86, 29);
-            this.textBox1.TabIndex = 8;
+            this.txtCM.Location = new System.Drawing.Point(42, 143);
+            this.txtCM.Name = "txtCM";
+            this.txtCM.Size = new System.Drawing.Size(86, 29);
+            this.txtCM.TabIndex = 8;
             // 
-            // textBox2
+            // txtM
             // 
-            this.textBox2.Location = new System.Drawing.Point(164, 143);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(86, 29);
-            this.textBox2.TabIndex = 9;
+            this.txtM.Location = new System.Drawing.Point(164, 143);
+            this.txtM.Name = "txtM";
+            this.txtM.Size = new System.Drawing.Size(86, 29);
+            this.txtM.TabIndex = 9;
             // 
-            // textBox3
+            // txtKM
             // 
-            this.textBox3.Location = new System.Drawing.Point(276, 143);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(86, 29);
-            this.textBox3.TabIndex = 10;
+            this.txtKM.Location = new System.Drawing.Point(276, 143);
+            this.txtKM.Name = "txtKM";
+            this.txtKM.Size = new System.Drawing.Size(86, 29);
+            this.txtKM.TabIndex = 10;
             // 
-            // textBox4
+            // txtYard
             // 
-            this.textBox4.Location = new System.Drawing.Point(276, 229);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(86, 29);
-            this.textBox4.TabIndex = 13;
+            this.txtYard.Location = new System.Drawing.Point(276, 229);
+            this.txtYard.Name = "txtYard";
+            this.txtYard.Size = new System.Drawing.Size(86, 29);
+            this.txtYard.TabIndex = 13;
             // 
-            // textBox5
+            // txtFt
             // 
-            this.textBox5.Location = new System.Drawing.Point(164, 229);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(86, 29);
-            this.textBox5.TabIndex = 12;
+            this.txtFt.Location = new System.Drawing.Point(164, 229);
+            this.txtFt.Name = "txtFt";
+            this.txtFt.Size = new System.Drawing.Size(86, 29);
+            this.txtFt.TabIndex = 12;
             // 
-            // textBox6
+            // txtIn
             // 
-            this.textBox6.Location = new System.Drawing.Point(42, 229);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(86, 29);
-            this.textBox6.TabIndex = 11;
+            this.txtIn.Location = new System.Drawing.Point(42, 229);
+            this.txtIn.Name = "txtIn";
+            this.txtIn.Size = new System.Drawing.Size(86, 29);
+            this.txtIn.TabIndex = 11;
             // 
-            // textBox7
+            // txtInfo
             // 
-            this.textBox7.Location = new System.Drawing.Point(42, 316);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(320, 29);
-            this.textBox7.TabIndex = 14;
+            this.txtInfo.Location = new System.Drawing.Point(42, 316);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(320, 29);
+            this.txtInfo.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 455);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtYard);
+            this.Controls.Add(this.txtFt);
+            this.Controls.Add(this.txtIn);
+            this.Controls.Add(this.txtKM);
+            this.Controls.Add(this.txtM);
+            this.Controls.Add(this.txtCM);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAllClear);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -205,19 +205,19 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnAllClear;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtCM;
+        private System.Windows.Forms.TextBox txtM;
+        private System.Windows.Forms.TextBox txtKM;
+        private System.Windows.Forms.TextBox txtYard;
+        private System.Windows.Forms.TextBox txtFt;
+        private System.Windows.Forms.TextBox txtIn;
+        private System.Windows.Forms.TextBox txtInfo;
     }
 }
 
